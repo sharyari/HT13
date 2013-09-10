b0451a32357c5db983e12f9ae6c5213768bd5458
sharyari <sharyari@gmail.com>
2013-09-10 19:53:43 +0200

    Fixed last exercise.

--- RT/Ass1/intbuffertwo.adb
+++ RT/Ass1/intbuffertwo.adb
@@ -19,54 +19,36 @@ procedure Intbuffertwo is
    end Producer;   
       
    protected type Buffer is
-      function Read return Integer;
-      procedure Write(I : Integer);
-      procedure PR_Increase;
+      entry Read(I : out Integer);
+      entry Write(I : Integer);
    private
       --function Pread return Integer;
       --procedure Pwrite(I : Integer);
       B : Buffer_Array;
       PW : Integer := 0;
-      PR : Integer := -1;
+      PR : Integer := 0;
+      D : Integer := 0;
       Q : Boolean := False;
    end Buffer;
    
    protected body Buffer is
       
-      --procedure Write(I : Integer) is
-      --begin
-	 --Pwrite(I);
-      --end Write;
-      
-      --function Read return Integer is
-      --begin
-	-- return Pread;
-      --end Read;
-      
-      procedure PR_Increase is
+      entry Write(I : Integer) 
+      when D < 20 is
       begin
-	 if (PR mod 20 /= PW) then
-	    PR := (PR + 1) mod 20;
-	 end if;
-      end PR_Increase;
-      
-      procedure Write(I : Integer) is
-      begin
-	 if ((PW+1) mod 20 /= PR) then
-	    B(PW) := I;
-	    Put_Line("  Buffer: Writing " & Integer'Image(I) & " at position " & Integer'Image(PW) & "");
-	    PW := (PW + 1) mod 20;
-	 end if;
+	 B(PW) := I;
+	 Put_Line("  Buffer: Writing " & Integer'Image(I) & " at position " & Integer'Image(PW) & "");
+	 PW := (PW + 1) mod 20;
+	 D := D + 1;
       end Write;
       
-      function Read return Integer is
+      entry Read(I : out Integer) --return Integer
+      when D /= 0 is
       begin
-	 --if (PR mod 20 /= PW) then
-	    --   delay 0.1;
-	    --end loop;
 	 Put_Line("  Buffer: Reading " & Integer'Image(B(PR)) & " from position " & Integer'Image(PR) & "");
-	 return B(PR);	 
-	 --end if;
+	 I:= B(PR);	 
+	 PR := (PR + 1) mod 20;
+	 D := D - 1;
       end Read;	 
    end Buffer;
    
@@ -108,9 +90,8 @@ procedure Intbuffertwo is
       Reset(G);
       
       while (C < 100) loop
-	 delay Duration(Float(Random(G)) / 100.0);
-	 B.PR_Increase;
-	 I := B.Read;
+	 delay Duration(Float(Random(G)) / 1000.0);
+	 B.Read(I);
 	 C := C + I;
 	 Put_Line("Consumer read" & Integer'Image(I) & ".");
       end loop;
