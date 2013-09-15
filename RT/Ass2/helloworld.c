e2b7bfcceb4a820010894ca8ed31913b3c7ab01d
Jonathan Sharyari <josh0151@beurling.it.uu.se>
2013-09-15 15:07:04 +0200

    completed part 1.

--- RT/Ass2/helloworld.c
+++ RT/Ass2/helloworld.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
@@ -8,18 +9,35 @@
 DeclareTask(HelloworldTask);
 
 
-void ecrobot_device_initialize() {}
-void ecrobot_device_terminate() {}
+void ecrobot_device_initialize() {
+  ecrobot_set_light_sensor_active(NXT_PORT_S1);
+}
+void ecrobot_device_terminate() {
+  ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
+}
 void user_1ms_isr_type2(void) {}
 
 
 
 TASK(HelloworldTask)
 {
+  boolean a = true;
+  display_clear(1);
+  while (true) {
+    display_string("Hello World!\n");
+    if (a)
+      display_string("Din mamma\n");
+    a = !a;
+    int number = ecrobot_get_light_sensor(NXT_PORT_S1);
+    char t[10];
+    for (int i = 5; i >= 0; i--) {
+      t[i] = number % 10 + 48;
+      number /= 10;
+    }
+    display_string(t);
+    display_update();
+    systick_wait_ms(100);
+  }
 
-    // Your code comes here
-	display_clear(1);
-	display_string("Hello World!");
-	display_update();
-    TerminateTask();
+  TerminateTask();
 }
